a0f038476d83bb3c8cc7f4b3f6007ec267163524
efe topalak <efetopalak@efe-MacBook-Pro.local>
2026-03-07 12:14:56 +0300

    feat: added templates

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+def home(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request=request, template_name='blog/home.html', context=context)
+    #request is coming http request from the user's browser
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request=request, template_name='blog/about.html', context={'title': 'About'})
 # Create your views here.
--- app/settings/settings.py
+++ app/settings/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wqsf6-2e&wt^_hggam8mkrwg9n8ey6*(km9iz*3+%+mdgw5y=g'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,13 +34,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
 ]
 
 MIDDLEWARE = [
